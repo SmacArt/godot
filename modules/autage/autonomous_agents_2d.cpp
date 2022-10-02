cd83835ccfa360e63c3a0409addb5bebc1741f36
Steve <Smac_Art@hotmail.com>
2022-10-02 13:20:58 +1000

    continue autonomousagent2d

--- modules/autage/autonomous_agents_2d.cpp
+++ modules/autage/autonomous_agents_2d.cpp
@@ -1,5 +1,7 @@
 #include "autonomous_agents_2d.h"
 
+#include "core/core_string_names.h"
+
 void AutonomousAgents2D::set_amount(int p_amount) {
 	ERR_FAIL_COND_MSG(p_amount < 1, "Amount of agents must be greater than 0.");
 
@@ -25,7 +27,7 @@ void AutonomousAgents2D::set_running(bool p_running) {
   if (p_running != running) {
     running = p_running;
     if (running) {
-      iset_process_internal(true);
+      set_process_internal(true);
     }
   }
 }
@@ -62,6 +64,99 @@ void AutonomousAgents2D::_texture_changed() {
 		queue_redraw();
 		_update_mesh_texture();
 	}
+}
+void AutonomousAgents2D::_set_do_redraw(bool p_do_redraw) {
+	if (do_redraw == p_do_redraw) {
+		return;
+	}
+	do_redraw = p_do_redraw;
+
+	{
+		MutexLock lock(update_mutex);
+
+		if (do_redraw) {
+			RS::get_singleton()->connect("frame_pre_draw", callable_mp(this, &AutonomousAgents2D::_update_render_thread));
+			RS::get_singleton()->canvas_item_set_update_when_visible(get_canvas_item(), true);
+
+			RS::get_singleton()->multimesh_set_visible_instances(multimesh, -1);
+		} else {
+			if (RS::get_singleton()->is_connected("frame_pre_draw", callable_mp(this, &AutonomousAgents2D::_update_render_thread))) {
+				RS::get_singleton()->disconnect("frame_pre_draw", callable_mp(this, &AutonomousAgents2D::_update_render_thread));
+			}
+			RS::get_singleton()->canvas_item_set_update_when_visible(get_canvas_item(), false);
+
+			RS::get_singleton()->multimesh_set_visible_instances(multimesh, 0);
+		}
+	}
+
+	queue_redraw(); // redraw to update render list
+}
+
+void AutonomousAgents2D::_update_internal() {
+	if (agents.size() == 0 || !is_visible_in_tree()) {
+		_set_do_redraw(false);
+		return;
+	}
+
+	double delta = get_process_delta_time();
+	if (running) {
+		inactive_time = 0;
+	} else {
+		inactive_time += delta;
+		if (inactive_time > lifetime * 1.2) {
+			set_process_internal(false);
+			_set_do_redraw(false);
+
+			//reset variables
+			time = 0;
+			inactive_time = 0;
+			frame_remainder = 0;
+			cycle = 0;
+			return;
+		}
+	}
+	_set_do_redraw(true);
+
+	if (time == 0 && pre_process_time > 0.0) {
+		double frame_time;
+		if (fixed_fps > 0) {
+			frame_time = 1.0 / fixed_fps;
+		} else {
+			frame_time = 1.0 / 30.0;
+		}
+
+		double todo = pre_process_time;
+
+		while (todo >= 0) {
+			_agents_process(frame_time);
+			todo -= frame_time;
+		}
+	}
+
+	if (fixed_fps > 0) {
+		double frame_time = 1.0 / fixed_fps;
+		double decr = frame_time;
+
+		double ldelta = delta;
+		if (ldelta > 0.1) { //avoid recursive stalls if fps goes below 10
+			ldelta = 0.1;
+		} else if (ldelta <= 0.0) { //unlikely but..
+			ldelta = 0.001;
+		}
+		double todo = frame_remainder + ldelta;
+
+		while (todo >= frame_time) {
+			_agents_process(frame_time);
+			todo -= decr;
+		}
+
+		frame_remainder = todo;
+
+	} else {
+		_agents_process(delta);
+	}
+
+	_update_agent_data_buffer();
 }
 
 void AutonomousAgents2D::_update_mesh_texture() {
@@ -115,6 +210,11 @@ void AutonomousAgents2D::_update_mesh_texture() {
 	RS::get_singleton()->mesh_clear(mesh);
 	RS::get_singleton()->mesh_add_surface_from_arrays(mesh, RS::PRIMITIVE_TRIANGLES, arr);
 
+}
+
+void AutonomousAgents2D::_update_render_thread() {
+  MutexLock lock(update_mutex);
+  RS::get_singleton()->multimesh_set_buffer(multimesh, agent_data);
 }
 
 void AutonomousAgents2D::_notification(int p_what) {
@@ -186,15 +286,22 @@ void AutonomousAgents2D::_notification(int p_what) {
 void AutonomousAgents2D::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("set_amount", "amount"), &AutonomousAgents2D::set_amount);
+	ClassDB::bind_method(D_METHOD("set_lifetime", "lifetime"), &AutonomousAgents2D::set_lifetime);
+	ClassDB::bind_method(D_METHOD("set_pre_process_time", "pre_process_time"), &AutonomousAgents2D::set_pre_process_time);
 	ClassDB::bind_method(D_METHOD("set_running", "running"), &AutonomousAgents2D::set_running);
 	ClassDB::bind_method(D_METHOD("set_texture", "texture"), &AutonomousAgents2D::set_texture);
 
 	ClassDB::bind_method(D_METHOD("get_amount"), &AutonomousAgents2D::get_amount);
-	ClassDB::bind_method(D_METHOD("get_texture"), &AutonomousAgents2D::get_texture);
+	ClassDB::bind_method(D_METHOD("get_lifetime"), &AutonomousAgents2D::get_lifetime);
+	ClassDB::bind_method(D_METHOD("get_pre_process_time"), &AutonomousAgents2D::get_pre_process_time);
 	ClassDB::bind_method(D_METHOD("is_running"), &AutonomousAgents2D::is_running);
+	ClassDB::bind_method(D_METHOD("get_texture"), &AutonomousAgents2D::get_texture);
 
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "running"), "set_running", "is_running");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "amount", PROPERTY_HINT_RANGE, "1,1000000,1,exp"), "set_amount", "get_amount");
+  ADD_GROUP("Time","");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "lifetime", PROPERTY_HINT_RANGE, "0.01,600.0,0.01,or_greater,suffix:s"), "set_lifetime", "get_lifetime");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "preprocess", PROPERTY_HINT_RANGE, "0.00,600.0,0.01,suffix:s"), "set_pre_process_time", "get_pre_process_time");
   ADD_GROUP("Drawing","");
 	// No visibility_rect property contrarily to Particles2D, it's updated automatically.
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "local_coords"), "set_use_local_coordinates", "get_use_local_coordinates");
--- modules/autage/autonomous_agents_2d.h
+++ modules/autage/autonomous_agents_2d.h
@@ -15,7 +15,6 @@ public:
 
 private:
 
-  bool running = false;
 
   struct Agent {
     Transform2D transform;
@@ -25,6 +24,11 @@ private:
     bool active = false;
   };
 
+  bool do_redraw = false;
+  double inactive_time = 0.0;
+  double frame_remainder = 0.0;
+  bool running = false;
+
   RID mesh;
   RID multimesh;
 
@@ -32,27 +36,34 @@ private:
   Vector<float> agent_data;
   Vector<int> agent_order;
 
+  double lifetime = 1.0;
+  double pre_process_time = 0.0;
   bool local_coords = false;
   DrawOrder draw_order = DRAW_ORDER_INDEX;
 
   Ref<Texture2D> texture;
+  Mutex update_mutex;
 
+  void _update_internal();
   void _update_mesh_texture();
+  void _update_render_thread();
+  void _set_do_redraw(bool p_do_redraw);
   void _texture_changed();
 
 public:
   void set_amount(int p_amount);
   void set_draw_order(DrawOrder p_order);
+  void set_lifetime(double p_lifetime);
+  void set_pre_process_time(double p_time);
   void set_running(bool p_emitting);
   void set_texture(const Ref<Texture2D> &p_texture);
 
   int get_amount() const;
   DrawOrder get_draw_order() const;
-  Ref<Texture2D> get_texture() const;
+  double get_lifetime() const;
+  double get_pre_process_time() const;
   bool is_running() const;
-
-  void _update_mesh_texture();
-
+  Ref<Texture2D> get_texture() const;
 
 protected:
   static void _bind_methods();
@@ -64,6 +75,6 @@ protected:
 
 };
 
-VARIANT_ENUM_CAST(AnonomousAgents2D::DrawOrder)
+VARIANT_ENUM_CAST(AutonomousAgents2D::DrawOrder)
 
 #endif
