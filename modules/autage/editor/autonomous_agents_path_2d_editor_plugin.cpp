9f49979e052c354dd6273c446ca5699823713130
Steve <Smac_Art@hotmail.com>
2023-01-21 06:27:07 +1000

    fix editor pluggin to suit changes to base

--- modules/autage/editor/autonomous_agents_path_2d_editor_plugin.cpp
+++ modules/autage/editor/autonomous_agents_path_2d_editor_plugin.cpp
@@ -90,7 +90,7 @@ bool AutonomousAgentsPath2DEditor::forward_gui_input(const Ref<InputEvent> &p_ev
 				}
 
 				// Check for point deletion.
-				Ref<EditorUndoRedoManager> &undo_redo = EditorNode::get_undo_redo();
+        EditorUndoRedoManager *undo_redo = EditorUndoRedoManager::get_singleton();
 				if ((mb->get_button_index() == MouseButton::RIGHT && mode == MODE_EDIT) || (mb->get_button_index() == MouseButton::LEFT && mode == MODE_DELETE)) {
 					if (dist_to_p < grab_threshold) {
 						undo_redo->create_action(TTR("Remove Point from Curve"));
@@ -125,7 +125,7 @@ bool AutonomousAgentsPath2DEditor::forward_gui_input(const Ref<InputEvent> &p_ev
 		if (mb->is_pressed() && mb->get_button_index() == MouseButton::LEFT && ((mb->is_command_or_control_pressed() && mode == MODE_EDIT) || mode == MODE_CREATE)) {
 			Ref<Curve2D> curve = node->get_autonomous_agents_path()->get_curve();
 
-			Ref<EditorUndoRedoManager> &undo_redo = EditorNode::get_undo_redo();
+      EditorUndoRedoManager *undo_redo = EditorUndoRedoManager::get_singleton();
 			undo_redo->create_action(TTR("Add Point to Curve"));
 			undo_redo->add_do_method(curve.ptr(), "add_point", cpoint);
 			undo_redo->add_undo_method(curve.ptr(), "remove_point", curve->get_point_count());
@@ -161,7 +161,7 @@ bool AutonomousAgentsPath2DEditor::forward_gui_input(const Ref<InputEvent> &p_ev
 				insertion_point = curve->get_point_count() - 2;
 			}
 
-			Ref<EditorUndoRedoManager> &undo_redo = EditorNode::get_undo_redo();
+      EditorUndoRedoManager *undo_redo = EditorUndoRedoManager::get_singleton();
 			undo_redo->create_action(TTR("Split Curve"));
 			undo_redo->add_do_method(curve.ptr(), "add_point", xform.affine_inverse().xform(gpoint2), Vector2(0, 0), Vector2(0, 0), insertion_point + 1);
 			undo_redo->add_undo_method(curve.ptr(), "remove_point", insertion_point + 1);
@@ -185,7 +185,7 @@ bool AutonomousAgentsPath2DEditor::forward_gui_input(const Ref<InputEvent> &p_ev
 		if (!mb->is_pressed() && mb->get_button_index() == MouseButton::LEFT && action != ACTION_NONE) {
 			Ref<Curve2D> curve = node->get_autonomous_agents_path()->get_curve();
 
-			Ref<EditorUndoRedoManager> &undo_redo = EditorNode::get_undo_redo();
+      EditorUndoRedoManager *undo_redo = EditorUndoRedoManager::get_singleton();
 			Vector2 new_pos = moving_from + xform.affine_inverse().basis_xform(gpoint - moving_screen_from);
 			switch (action) {
 				case ACTION_NONE:
@@ -464,7 +464,7 @@ void AutonomousAgentsPath2DEditor::_mode_selected(int p_mode) {
 			return;
 		}
 
-		Ref<EditorUndoRedoManager> &undo_redo = EditorNode::get_undo_redo();
+    EditorUndoRedoManager *undo_redo = EditorUndoRedoManager::get_singleton();
 		undo_redo->create_action(TTR("Remove Point from Curve"));
 		undo_redo->add_do_method(node->get_autonomous_agents_path()->get_curve().ptr(), "add_point", begin);
 		undo_redo->add_undo_method(node->get_autonomous_agents_path()->get_curve().ptr(), "remove_point", node->get_autonomous_agents_path()->get_curve()->get_point_count());
